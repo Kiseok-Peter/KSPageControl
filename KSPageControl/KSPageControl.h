77ac0457448791704bf4c4139b3a896f9673acab
Peter <gkfnrkfl@naver.com>
2020-01-28 13:59:39 +0900

    Initial Commit

---
+++ KSPageControl/KSPageControl.h
@@ -0,0 +1,19 @@
+//
+//  KSPageControl.h
+//  KSPageControl
+//
+//  Created by 박기석 on 2020/01/28.
+//  Copyright © 2020 Peter. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KSPageControl.
+FOUNDATION_EXPORT double KSPageControlVersionNumber;
+
+//! Project version string for KSPageControl.
+FOUNDATION_EXPORT const unsigned char KSPageControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KSPageControl/PublicHeader.h>
+
+
